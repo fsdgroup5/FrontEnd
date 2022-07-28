cb9c1e2fe66dfc087839fd43a21dc178c1c55a4b
Deepakvijayan07 <deepakvijayan31@gmail.com>
2022-07-28 13:50:18 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditHallComponent } from './edit-hall/edit-hall.component';
+import { HallComponent } from './hall/hall.component';
+import { NewhallComponent } from './newhall/newhall.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'AddHall',component:NewhallComponent},
+  {path:'update',component:EditHallComponent},
+  {path:'',component:HallComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewhallComponent } from './newhall/newhall.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { HallComponent } from './hall/hall.component';
+import { HeaderComponent } from './header/header.component';
+import { EditHallComponent } from './edit-hall/edit-hall.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewhallComponent,
+    HallComponent,
+    HeaderComponent,
+    EditHallComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
